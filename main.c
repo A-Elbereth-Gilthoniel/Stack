4d3da5e8afe29da4d89e51853fcd3b60903e5e3b
Rinat Rakhmetov <rakhmetoff.rinat@yandex.ru>
2024-03-23 20:01:51 +0300

    added README

--- main.c
+++ main.c
@@ -2,28 +2,17 @@
 
 int main()
 {
-    STACK stk = {};
+    STACK* stk = (STACK*) calloc(1, sizeof(STACK));
+    StackConstructor(stk, 10);
     ELEM a = 0;
-    StackConstructor(&stk, 5);
-    StackPush(&stk, 20);
-    StackPush(&stk, 3);
-    StackPush(&stk, 3);
-    StackPush(&stk, 1);
-    StackPush(&stk, 7);
     for (double i = 1; i < 100; i++)
-        StackPush(&stk, i);
+        StackPush(stk, i);
 
     for (int j = 0; j < 95; j++)
     {
-        a = StackPop(&stk);
+        a = StackPop(stk);
     }
-    //a = StackPop(stk);
-    printf(SPEC" aka\n", a);
-
-
-    PrintStack(&stk);
-    StackDestructor(&stk);
+    PrintStack(stk);
+    StackDestructor(stk);
     return 0;
-
-
 }
